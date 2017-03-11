886311a25bb37ec3788286ff44e1059e02e194e1
surajgupta <suraj.gupta.489@gmail.com>
2017-03-12 02:15:32 +0530

    initial commit

---
+++ taxonomy-listing-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Taxonomy Listing Plugin
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: @surajprgupta
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/

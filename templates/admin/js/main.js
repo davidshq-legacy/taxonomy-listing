139a58638af1d4aa80032316b43ddb59123e1efc
surajgupta <suraj.gupta.489@gmail.com>
2017-04-03 20:01:04 +0530

    clear select 2 values on close and insert shortcode_button event

--- templates/admin/js/main.js
+++ templates/admin/js/main.js
@@ -3,6 +3,15 @@ var scrptz_tdl_select2 = $.fn.select2;
 
 $(document).ready(function () {
     scrptz_tdl_select2.call($(".scrptz-tdl-select2"), {
-        allowClear: true,
+        allowClear: true
     });
+
+    $(document).on('shortcode_button:close', function(e){
+        scrptz_tdl_select2.call($(".scrptz-tdl-select2").val(""));
+    });
+
+    $(document).on('shortcode_button:insert', function(e){
+        scrptz_tdl_select2.call($(".scrptz-tdl-select2").val(""));
+    });
+
 });
